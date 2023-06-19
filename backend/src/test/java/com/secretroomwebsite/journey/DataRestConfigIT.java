948f3e70230933e6e58d0a5b3c065ac8d23a6264
Slava Zapolschi <zapolski.slava@gmail.com>
2023-06-19 22:04:54 +0300

    added IT test to DataRestConfig

---
+++ backend/src/test/java/com/secretroomwebsite/journey/DataRestConfigIT.java
@@ -0,0 +1,12 @@
+package com.secretroomwebsite.config;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataRestConfigIT {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
