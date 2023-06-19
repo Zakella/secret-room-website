7e20c7fd687868f7da28c09d0acad1109e012a55
Zakella <zapolski.slava@gmail.com>
2023-06-19 14:11:30 +0300

    initial project

---
+++ backend/src/test/java/com/secretroomwebsite/SecretroomWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secretroomwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretroomWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
