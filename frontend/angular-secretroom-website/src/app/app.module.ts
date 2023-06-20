12d30e67cccdaafa16e60be719b9f4c78ce331d0
Slava Zapolschi <zapolski.slava@gmail.com>
2023-06-21 00:12:29 +0300

    added template , header, subheader

--- frontend/angular-secretroom-website/src/app/app.module.ts
+++ frontend/angular-secretroom-website/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {ProductService} from "./services/product.service";
+import { HeaderComponent } from './components/header/header.component';
+import { SubHeaderComponent } from './components/sub-header/sub-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    HeaderComponent,
+    SubHeaderComponent
   ],
   imports: [
     BrowserModule,
