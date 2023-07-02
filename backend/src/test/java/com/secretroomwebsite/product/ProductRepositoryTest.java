48d7d30cc572a85aa338f960fcd0b3017aa9502e
Slava Zapolschi <zapolski.slava@gmail.com>
2023-07-02 22:20:33 +0300

    backend: - added test itShouldFindAllByProductCategory_Id frontend:

--- backend/src/test/java/com/secretroomwebsite/product/ProductRepositoryTest.java
+++ backend/src/test/java/com/secretroomwebsite/product/ProductRepositoryTest.java
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -34,10 +37,50 @@ class ProductRepositoryTest  extends AbstractTestcontainers {
 
     @Test
     void itShouldFindAllByProductCategory_Id() {
-        //given
-        //when
-        //then
 
+        ProductCategory category = new ProductCategory();
+        category.setDescription("Category A");
+        category.setBrand(Brands.VictoriasSecret);
+        category.setCategoryName("Category A");
+        category.setImageUrl("assets/tests");
+        productCategoryRepository.save(category);
+
+        Product product1 = Product.builder()
+                .sku("SKU001")
+                .productCategory(category)
+                .name("Product 1")
+                .description("Description 1")
+                .brand(Brands.VictoriasSecret)
+                .shortDescription("Short Description 1")
+                .unitPrice(10.0)
+                .imageURL("image1.jpg")
+                .active(true)
+                .unitsInStock(100)
+                .dateCreated(LocalDate.now())
+                .build();
+        underTest.save(product1);
+
+        Product product2 = Product.builder()
+                .sku("SKU002")
+                .productCategory(category)
+                .name("Product 2")
+                .description("Description 2")
+                .brand(Brands.VictoriasSecret)
+                .shortDescription("Short Description 2")
+                .unitPrice(15.0)
+                .imageURL("image2.jpg")
+                .active(true)
+                .unitsInStock(50)
+                .dateCreated(LocalDate.now())
+                .build();
+        underTest.save(product2);
+
+        Pageable pageable = PageRequest.of(0, 10); // Example: First page with 10 items per page
+        Page<Product> productsByCategory = underTest.findAllByProductCategory_Id(category.getId(), pageable);
+
+        assertNotNull(productsByCategory);
+        assertFalse(productsByCategory.isEmpty());
+        assertEquals(2, productsByCategory.getContent().size());
     }
 
     @Test
@@ -49,8 +92,6 @@ class ProductRepositoryTest  extends AbstractTestcontainers {
         category.setCategoryName("Category A");
         category.setImageUrl("assets/tests");
         productCategoryRepository.save(category);
-
-
 
         Product product = Product.builder()
                 .sku("SKU001")
@@ -67,6 +108,13 @@ class ProductRepositoryTest  extends AbstractTestcontainers {
                 .build();
 
         underTest.save(product);
+
+        Pageable pageable = PageRequest.of(0, 10); // Example: First page with 10 items per page
+        Page<Product> productsByBrand = underTest.findByBrand(Brands.VictoriasSecret, pageable);
+
+        assertNotNull(productsByBrand);
+        assertFalse(productsByBrand.isEmpty());
+        assertEquals(1, productsByBrand.getContent().size());
 
 
     }
