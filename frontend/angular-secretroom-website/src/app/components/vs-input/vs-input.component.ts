beb29dce7cc954431cba525b63db86656c04b96e
Slava Zapolschi <zapolski.slava@gmail.com>
2023-08-02 23:18:27 +0300

    VsInputComponent now implements the ControlValueAccessor interface. This interface is required for custom form controls to work with Angular forms.
    Added NG_VALUE_ACCESSOR provider to the VsInputComponent to allow Angular to recognize it as a custom form control.
    
    Added getter and setter for value with associated onChange and onTouch methods in the VsInputComponent.
    
    Updated the template of VsInputComponent to use two-way data binding with ngModel for the value of the input field.
    
    These changes allow VsInputComponent to be used as a form control in any Angular form.

--- frontend/angular-secretroom-website/src/app/components/vs-input/vs-input.component.ts
+++ frontend/angular-secretroom-website/src/app/components/vs-input/vs-input.component.ts
@@ -1,12 +1,46 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-vs-input',
   templateUrl: './vs-input.component.html',
-  styleUrls: ['./vs-input.component.css']
+  styleUrls: ['./vs-input.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => VsInputComponent),
+      multi: true
+    }
+  ]
 })
-export class VsInputComponent {
+export class VsInputComponent implements ControlValueAccessor {
   @Input() label!: string;
   @Input() id!: string;
   @Input() type: string = 'text';
+
+  private _value: any = '';
+  onChange: any = () => {};
+  onTouch: any = () => {};
+
+  get value() {
+    return this._value;
+  }
+
+  set value(val) {
+    this._value = val;
+    this.onChange(val);
+    this.onTouch(val);
+  }
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
 }
